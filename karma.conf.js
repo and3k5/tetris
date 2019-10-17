7c0b9b72ee5ce2cd2f2427a0b9fc4247342376c6
Anders K <ande7353@gmail.com>
2019-10-17 12:54:37 +0200

    Add brick unit testing

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+var webpackConfig = require("./webpack.config.js");
+console.log(webpackConfig);
+
+module.exports = (config) => {
+    config.set({
+        frameworks: ['jasmine'],
+        // ... normal karma configuration
+        files: [
+            // all files ending in "_test"
+            { pattern: 'src/test/*-test.js', watched: false },
+            { pattern: 'src/test/**/*-test.js', watched: false },
+            // each file acts as entry point for the webpack configuration
+        ],
+
+        preprocessors: {
+            // add webpack as preprocessor
+            'src/test/*-test.js': ['webpack'],
+            'src/test/**/*-test.js': ['webpack'],
+        },
+
+        webpack: webpackConfig({mode: "development"}),
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            stats: 'errors-only',
+        },
+    });
+};
---
+++ src/test/brick-test.js
@@ -0,0 +1,65 @@
+import Brick from "../tetris/brick.js";
+import TetrisGame from "../tetris/game.js";
+import { defaultGame, TetrisSetup } from "../tetris/game-setup.js";
+
+var setup = defaultGame();
+
+describe("brick", () => {
+
+    for (var brickform of setup.brickforms) {
+        var i = setup.brickforms.indexOf(brickform);
+
+        for (var j = 0; j < 4; j++) {
+            it(`has a proper moveLeft property on brickform ${i} when rotated ${j} times`, function () {
+                var i = this.i;
+                var setup = this.setup;
+                var brickform = this.brickform;
+
+                var testGame = new TetrisGame(setup);
+                testGame.nextRandom = i;
+
+                var brick = new Brick({ game: testGame, brickform: brickform, x: 3, y: 0 });
+                brick.moving = true;
+
+                for (var k = 0; k < j; k++)
+                    brick.rotate();
+
+                testGame.bricks.push(brick);
+
+                while (brick.canMoveLeft())
+                    brick.moveleft();
+
+                expect(brick.x).toEqual(brick.mostLeft);
+                console.log(brick.x, brick.mostLeft);
+            }.bind({ i, setup, brickform }))
+
+            it(`has a proper moveRight property on brickform ${i} when rotated ${j} times`, function () {
+                var i = this.i;
+                var setup = this.setup;
+                var brickform = this.brickform;
+
+                var testGame = new TetrisGame(setup);
+                testGame.nextRandom = i;
+
+                var brick = new Brick({ game: testGame, brickform: brickform, x: 3, y: 0 });
+                brick.moving = true;
+
+                for (var k = 0; k < j; k++)
+                    brick.rotate();
+
+                testGame.bricks.push(brick);
+
+                while (brick.canMoveRight())
+                    brick.moveright();
+
+                expect(brick.x).toEqual(brick.mostRight);
+                console.log(brick.x, brick.mostRight);
+            }.bind({ i, setup, brickform }))
+
+
+        }
+
+    }
+    it("has a proper moveLeft property")
+
+});
