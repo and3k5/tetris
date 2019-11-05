80b591d5294412d2a9fcbd5a3d1e52e3265164c6
Anders Kjeldsen <ande7353@gmail.com>
2019-11-05 07:03:06 +0100

    Getting a bit closer

--- src/tetris/flame.js
+++ src/tetris/flame.js
@@ -5,19 +5,47 @@ export class vec2 {
     }
 
     static add(a,b) {
+        if (typeof(a) === "number") {
+            return new vec2(a + b.x, a + b.y);
+        }
+        if (typeof(b) === "number") {
+            return new vec2(a.x + b, a.y + b);
+        }
         return new vec2(a.x + b.x, a.y + b.y);
     }
 
     static sub(a,b) {
+        if (typeof(a) === "number") {
+            return new vec2(a - b.x, a - b.y);
+        }
+        if (typeof(b) === "number") {
+            return new vec2(a.x - b, a.y - b);
+        }
         return new vec2(a.x - b.x, a.y - b.y);
     }
 
     static mul(a,b) {
+        if (typeof(a) === "number") {
+            return new vec2(a * b.x, a * b.y);
+        }
+        if (typeof(b) === "number") {
+            return new vec2(a.x * b, a.y * b);
+        }
         return new vec2(a.x * b.x, a.y * b.y);
     }
 
     static div(a,b) {
+        if (typeof(a) === "number") {
+            return new vec2(a / b.x, a / b.y);
+        }
+        if (typeof(b) === "number") {
+            return new vec2(a.x / b, a.y / b);
+        }
         return new vec2(a.x / b.x, a.y / b.y);
+    }
+
+    floor() {
+        return new vec2(floor(this.x), floor(this.y));
     }
 
     get xy() {
@@ -26,6 +54,10 @@ export class vec2 {
 
     dot(b) {
         return this.x * b.x + this.y * b.y;
+    }
+
+    sin() {
+        return new vec2(sin(this.x), sin(this.y));
     }
 }
 
@@ -37,19 +69,47 @@ export class vec3 {
     }
 
     static add(a,b) {
-        return new vec2(a.x + b.x, a.y + b.y, a.z + b.z);
+        if (typeof(a) === "number") {
+            return new vec3(a + b.x, a + b.y, a + b.z);
+        }
+        if (typeof(b) === "number") {
+            return new vec3(a.x + b, a.y + b, a.z + b);
+        }
+        return new vec3(a.x + b.x, a.y + b.y, a.z + b.z);
     }
 
     static sub(a,b) {
-        return new vec2(a.x - b.x, a.y - b.y, a.z - b.z);
+        if (typeof(a) === "number") {
+            return new vec3(a - b.x, a - b.y, a - b.z);
+        }
+        if (typeof(b) === "number") {
+            return new vec3(a.x - b, a.y - b, a.z - b);
+        }
+        return new vec3(a.x - b.x, a.y - b.y, a.z - b.z);
     }
 
     static mul(a,b) {
-        return new vec2(a.x * b.x, a.y * b.y, a.z * b.z);
+        if (typeof(a) === "number") {
+            return new vec3(a * b.x, a * b.y, a * b.z);
+        }
+        if (typeof(b) === "number") {
+            return new vec3(a.x * b, a.y * b, a.z * b);
+        }
+        return new vec3(a.x * b.x, a.y * b.y, a.z * b.z);
     }
 
     static div(a,b) {
-        return new vec2(a.x / b.x, a.y / b.y, a.z / b.z);
+        if (typeof(a) === "number") {
+            return new vec3(a / b.x, a / b.y, a / b.z);
+        }
+        if (typeof(b) === "number") {
+            return new vec3(a.x / b, a.y / b, a.z / b);
+        }
+        return new vec3(a.x / b.x, a.y / b.y, a.z / b.z);
+    }
+
+    floor() {
+        return new vec3(floor(this.x), floor(this.y), floor(this.z));
     }
 
     get xy() {
@@ -58,6 +118,10 @@ export class vec3 {
 
     dot(b) {
         return this.x * b.x + this.y * b.y + this.z * b.z;
+    }
+
+    sin() {
+        return new vec3(sin(this.x), sin(this.y), sin(this.z));
     }
 }
 
@@ -69,13 +133,16 @@ class mat2 {
 
 function add(a,b) {
     if (a instanceof vec2 && b instanceof vec2) {
-        return a.add(b);
+        return vec2.add(a, b);
     }
     else if (a instanceof vec3 && b instanceof vec3) {
-        return a.add(b);
-    }else if (typeof(a) === "number" && typeof(b) === "number")
-    {
-        return a+b;
+        return vec3.add(a, b);
+    } else if (typeof (a) === "number" && typeof (b) === "number") {
+        return a + b;
+    } else if (typeof (a) === "number" && b instanceof vec2) {
+        return vec2.add(a,b);
+    } else if (a instanceof vec2 && typeof (b) === "number") {
+        return vec2.add(a,b);
     }else{
         var aType = typeof(a);
         if (aType == "object")
@@ -91,13 +158,43 @@ function add(a,b) {
 
 function mul(a,b) {
     if (a instanceof vec2 && b instanceof vec2) {
-        return a.mul(b);
+        return vec2.mul(a,b);
     }
     else if (a instanceof vec3 && b instanceof vec3) {
-        return a.mul(b);
+        return vec3.mul(a,b);
     }else if (typeof(a) === "number" && typeof(b) === "number")
     {
         return a*b;
+    } else if (typeof (a) === "number" && b instanceof vec2) {
+        return vec2.mul(a,b);
+    } else if (a instanceof vec2 && typeof (b) === "number") {
+        return vec2.mul(a,b);
+    }else{
+        var aType = typeof(a);
+        if (aType == "object")
+            aType = a.constructor.name;
+
+        var bType = typeof(b);
+        if (bType == "object")
+            bType = b.constructor.name;
+
+        throw new Error("unsupported: "+aType+ " + "+bType);
+    }
+}
+
+function sub(a,b) {
+    if (a instanceof vec2 && b instanceof vec2) {
+        return vec2.sub(a,b);
+    }
+    else if (a instanceof vec3 && b instanceof vec3) {
+        return vec3.sub(a,b);
+    }else if (typeof(a) === "number" && typeof(b) === "number")
+    {
+        return a-b;
+    } else if (typeof (a) === "number" && b instanceof vec2) {
+        return vec2.sub(a,b);
+    } else if (a instanceof vec2 && typeof (b) === "number") {
+        return vec2.sub(a,b);
     }else{
         var aType = typeof(a);
         if (aType == "object")
@@ -113,7 +210,37 @@ function mul(a,b) {
 
 const iResolution = new vec3(1,1,1);
 
-const floor = Math.floor.bind(Math);
+function floor(v) {
+    if (typeof(v) === "number")
+        return Math.floor(v);
+    if (v instanceof vec2)
+        return v.floor();
+    if (v instanceof vec3)
+        return v.floor();
+    throw new Exception("cannot use floor on unknown value");
+
+}
+
+function fract(x) {
+    if (typeof(x) === "number")
+        return x - floor(x);
+    if (x instanceof vec2)
+        return vec2.sub(x,floor(x));
+    if (x instanceof vec3)
+        return vec3.sub(x,floor(x));
+    throw new Error("unsupported fract");
+}
+
+function sin(x) {
+    if (typeof(x) === "number")
+        return Math.sin(x);
+    if (x instanceof vec2)
+        return x.sin();
+    if (x instanceof vec3)
+        return x.sin();
+    throw new Error("unsupported fract");
+}
+
 const max = Math.max.bind(Math);
 
 const iTime = 0;
@@ -183,22 +310,23 @@ function noise(p) {
     /**
      * @type {vec2}
      */
-    var b = a - o + K2;
+    var b = vec2.add(vec2.sub(a , o), K2);
 
     /**
      * @type {vec2}
      */
-    var c = a - 1.0 + 2.0 * K2;
+    var c = vec2.add(vec2.sub(a , 1.0) , (2.0 * K2));
 
     /**
      * @type {vec3}
      */
-    var h = max(0.5 - new vec3(dot(a, a), dot(b, b), dot(c, c)), 0.0);
+    var h = max(vec3.sub(0.5,new vec3(dot(a, a), dot(b, b), dot(c, c))), 0.0);
 
     /**
      * @type {vec3}
      */
-    var n = h * h * h * h * new vec3(dot(a, hash(i + 0.0)), dot(b, hash(i + o)), dot(c, hash(i + 1.0)));
+    
+    var n = vec3.mul(vec3.mul(vec3.mul(vec3.mul(h,h),h),h),new vec3(dot(a, hash(vec2.add(i,0.0))), dot(b, hash(vec2.add(i,o))), dot(c, hash(vec2.add(i, 1.0)))));
 
     return dot(n, new vec3(70.0));
 }
@@ -214,11 +342,11 @@ function fbm(uv) {
      * @type {mat2}
      */
     var m = new mat2(1.6, 1.2, -1.2, 1.6);
-    f = 0.5000 * noise(uv); uv = m * uv;
-    f += 0.2500 * noise(uv); uv = m * uv;
-    f += 0.1250 * noise(uv); uv = m * uv;
-    f += 0.0625 * noise(uv); uv = m * uv;
-    f = 0.5 + 0.5 * f;
+    f = vec2.mul(0.5000 , noise(uv)); uv = vec2.mul(m , uv);
+    f = vec2.add(f,vec2.mul(0.2500 , noise(uv))); uv = vec2.mul(m , uv);
+    f = vec2.add(f,vec2.mul(0.1250 , noise(uv))); uv = vec2.mul(m , uv);
+    f = vec2.add(f,vec2.mul(0.0625 , noise(uv))); uv = vec2.mul(m , uv);
+    f = vec2.add(0.5 , vec2.mul(0.5, f));
     return f;
 }
 
@@ -248,7 +376,7 @@ export function mainImage(fragColor, fragCoord) {
     q.x = mod(q.x, 1.) - 0.5;
     q.y -= 0.25;
 
-    var n = fbm(mul(strength , q).sub(new vec2(0, T3)));
+    var n = fbm(sub(mul(strength , q),(new vec2(0, T3))));
     var c = 1. - 16. * pow(max(0., length(q * new vec2(1.8 + q.y * 1.5, .75)) - n * max(0., q.y + .25)), 1.2);
     //	var c1 = n * c * (1.5-pow(1.25*uv.y,4.));
     var c1 = n * c * (1.5 - pow(2.50 * uv.y, 4.));
