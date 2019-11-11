7a34b9dd6dff11b8e7316b6473b5d6fdfae71592
Anders K <ande7353@gmail.com>
2019-11-11 12:16:12 +0100

    Fix hsla

--- src/tetris/color.js
+++ src/tetris/color.js
@@ -15,29 +15,50 @@ class Color {
     }
 
     toHSLA() {
-        var r = this.r / 255;
-        var g = this.r / 255;
-        var b = this.r / 255;
+        // make r, g, and b fractions of 1
+        let r = this.r / 255,
+            g = this.g / 255,
+            b = this.b / 255,
 
-        var max = Math.max(r, g, b), min = Math.min(r, g, b);
-        var h, s, l = (max + min) / 2;
+            // find greatest and smallest channel values
+            cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
 
-        if (max == min) {
-            h = s = 0; // achromatic
-        } else {
-            var d = max - min;
-            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        // calculate hue
+        // no difference
+        if (delta == 0)
+            h = 0;
+        // red is max
+        else if (cmax == r)
+            h = ((g - b) / delta) % 6;
+        // green is max
+        else if (cmax == g)
+            h = (b - r) / delta + 2;
+        // blue is max
+        else
+            h = (r - g) / delta + 4;
 
-            switch (max) {
-                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-                case g: h = (b - r) / d + 2; break;
-                case b: h = (r - g) / d + 4; break;
-            }
+        h = Math.round(h * 60);
 
-            h /= 6;
-        }
+        // make negative hues positive behind 360°
+        if (h < 0)
+            h += 360;
 
-        return { h, s, l, a: this.a };
+        // calculate lightness
+        l = (cmax + cmin) / 2;
+
+        // calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        // multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+
+        return { h, s: s / 100, l: l / 100, a: this.a };
     }
 
     setInvert() {
