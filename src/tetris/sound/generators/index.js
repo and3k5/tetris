29cbc81b9093883f4bd138d8404a69e6827173cd
Anders <and3k5@users.noreply.github.com>
2021-01-01 14:39:15 +0100

    make gamebump a generated sound

--- src/tetris/sound/generators/index.js
+++ src/tetris/sound/generators/index.js
@@ -1,8 +1,13 @@
 import gamelose from "./gamelose.js";
+import gamebump from "./gamebump.js";
 
 export default [
     {
         name: "gamelose",
         buffer: gamelose
+    },
+    {
+        name: "gamebump",
+        buffer: gamebump
     }
 ];
