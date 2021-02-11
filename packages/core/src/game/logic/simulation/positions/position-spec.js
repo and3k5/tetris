7a3b8906257e1bbc87b690ee4af99e0577c957c6
Anders <and3k5@users.noreply.github.com>
2021-02-11 16:14:27 +0100

    WIP: test positions

---
+++ packages/core/src/game/logic/simulation/positions/position-spec.js
@@ -0,0 +1,11 @@
+import { getPositions } from "."
+import { TetrisGame } from "../../../game"
+
+/*global describe, expect, it*/
+describe("getPositions", function () {
+    it("can get expected positions", function () {
+        throw new Error("TODO");
+        var game = new TetrisGame();
+        getPositions(game)
+    })
+})
