86743850b32915d0f06ef8205fc0e626bcf6a240
Anders <and3k5@users.noreply.github.com>
2021-02-18 12:31:05 +0100

    Improve sorting and rating

--- packages/core/src/game/logic/simulation/simulate-rating/index.js
+++ packages/core/src/game/logic/simulation/simulate-rating/index.js
@@ -70,20 +70,86 @@ export function countHeight(game, matrix) {
     return height;
 }
 
+export class Score {
+    constructor({ clearingLines, holes, height }) {
+        this.clearingLines = clearingLines;
+        this.holes = holes;
+        this.height = height;
+    }
+}
+
 export function getScores(game, matrix) {
     const clearingLines = countClearingLines(matrix);
     const holes = countHoles(game, matrix);
     const height = countHeight(game, matrix);
 
-    return {
+    return new Score({
         holes,
         height,
         clearingLines,
-    };
+    });
+}
+
+export function rate(worstScore, bestScore, currentScore, fieldName) {
+    const worst = worstScore[fieldName];
+    const best = bestScore[fieldName];
+    const current = currentScore[fieldName];
+
+    return ratioValue(worst, best, current);
+}
+
+export function ratioValue(worst, best, current) {
+    if (worst == best)
+        return 1;
+
+    let result = (current - worst) / (best - worst)
+
+    return result;
+}
+
+/**
+ * 
+ * @param {Score} score 
+ * @param {Score} worstScore 
+ * @param {Score} bestScore 
+ */
+function summaryScore(score, worstScore, bestScore) {
+    var scoreItems = [];
+    // 1 = 100% = good
+    // 0 = 0% = bad
+
+    scoreItems.push(rate(worstScore, bestScore, score, "clearingLines", true));
+    scoreItems.push(rate(worstScore, bestScore, score, "holes", false));
+    scoreItems.push(rate(worstScore, bestScore, score, "height", false));
+
+    const count = scoreItems.length;
+    const summary = scoreItems.reduce((a, b) => a + b, 0);
+
+    return summary / count;
+}
+
+function getWorstScoreValues(positions) {
+    positions = positions.concat();
+    const clearingLines = sortBy(s => s.scores.clearingLines, false).execute(positions)[0].scores.clearingLines;
+    const height = sortBy(s => s.scores.height, true).execute(positions)[0].scores.height;
+    const holes = sortBy(s => s.scores.holes, true).execute(positions)[0].scores.holes;
+    return new Score({ clearingLines, holes, height });
+}
+
+function getBestScoreValues(positions) {
+    positions = positions.concat();
+    const clearingLines = sortBy(s => s.scores.clearingLines, true).execute(positions)[0].scores.clearingLines;
+    const height = sortBy(s => s.scores.height, false).execute(positions)[0].scores.height;
+    const holes = sortBy(s => s.scores.holes, false).execute(positions)[0].scores.holes;
+    return new Score({ clearingLines, holes, height });
 }
 
 export function sortMovements(positions) {
-    return sortBy(s => s.scores.clearingLines, true)
+    const worstScore = getWorstScoreValues(positions);
+    const bestScore = getBestScoreValues(positions);
+
+    return sortBy(s => summaryScore(s.scores, worstScore, bestScore), true)
+        .thenBy(s => s.scores.clearingLines, true)
         .thenBy(s => s.scores.holes, false)
         .thenBy(s => s.scores.height, false)
         .execute(positions);
