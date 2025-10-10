7d264be1e467056f0155963af9d6b937d2e67e64
Anders <and3k5@users.noreply.github.com>
2025-10-10 23:06:35 +0200

    Fix base url

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ mode }) => ({
     plugins: [vue()],
+    base: "",
     define: {
         global: {
             development: mode === "development",
